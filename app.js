3d80811af84f561a7ef858ef1d13211d4a381da7
kavi307 <ajkavitha7@gmail.com>
2024-08-22 21:39:35 +0530

    namaste-react

---
+++ app.js
@@ -0,0 +1,16 @@
+const parent = React.createElement('div', {id : "parant"},
+[React.createElement('div', {id : "child"}, 
+    [React.createElement('h1', {} , "I'm H1 tag"), 
+     React.createElement('h2', {} , "I'm H2 tag")]),
+     React.createElement('div', {id : "child2"}, 
+    [React.createElement('h1', {} , "I'm H1 tag"), 
+     React.createElement('h2', {} , "I'm H2 tag")])
+    ]                
+)
+
+
+const heading = React.createElement('h1', {id:"head-element"}, "HelloWorld From React");
+
+const root = ReactDOM.createRoot(document.getElementById('Main-div'));
+
+root.render(parent)
